3938df89722182f0170c3bcf3b28edd17884c82a
Stefani Santos <111699269+StefaniSantos01@users.noreply.github.com>
2022-09-15 13:00:05 +0100

    Add files via upload

---
+++ Exercicio03.java
@@ -0,0 +1,24 @@
+package ExercicioIfElse;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Digite o seu genero: ");
+	String genero = scan.next();
+	 
+	 if(genero.equalsIgnoreCase("f")){
+		 System.out.println("F - Feminino");
+	 }else if (genero.equalsIgnoreCase("m")){
+		  System.out.println("M - Masculino");
+	  }else {
+		 System.out.println("Sexo invalido");
+	 }
+	
+
+	}
+
+}
